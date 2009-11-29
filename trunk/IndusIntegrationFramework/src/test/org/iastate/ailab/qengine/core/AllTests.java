2cf8f1700ead9efa9c0d322e2178c59ff425d3a8
neeraj.kaul <neeraj.kaul@c1094e34-ec05-11dd-b2d9-215335d0b316>
2009-11-29 04:17:29 +0000

    A Basic  Junit Suite Template to be used to run all tests

---
+++ trunk/IndusIntegrationFramework/src/test/org/iastate/ailab/qengine/core/AllTests.java
@@ -0,0 +1,15 @@
+package org.iastate.ailab.qengine.core;
+
+import org.iastate.ailab.qengine.SanityTestQueries;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * 
+ * @author neeraj
+ * 
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { SanityTestQueries.class })
+public class AllTests {
+}
