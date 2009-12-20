344fed6b02ac50b1f2680ff57e418e5d8e553146
neeraj.kaul <neeraj.kaul@c1094e34-ec05-11dd-b2d9-215335d0b316>
2009-12-20 17:52:22 +0000

    1. Bug fix for Pellet Reasoner to generate URi correctly by making calls to URI Utils. 2. Code clean with commenting of System.out.println code

--- trunk/IndusIntegrationFramework/src/java/org/iastate/ailab/qengine/core/reasoners/impl/PelletReasonerImpl.java
+++ trunk/IndusIntegrationFramework/src/java/org/iastate/ailab/qengine/core/reasoners/impl/PelletReasonerImpl.java
@@ -7,67 +7,68 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 import org.iastate.ailab.qengine.core.IndusConfiguration;
 import org.iastate.ailab.qengine.core.Init;
-
-import Zql.ZConstant;
-import Zql.ZExpression;
-
-
-//new
 import org.semanticweb.owl.apibinding.OWLManager;
 import org.semanticweb.owl.inference.OWLReasoner;
 import org.semanticweb.owl.inference.OWLReasonerAdapter;
 import org.semanticweb.owl.inference.OWLReasonerException;
 import org.semanticweb.owl.inference.OWLReasonerFactory;
-import org.semanticweb.owl.model.*;
+import org.semanticweb.owl.model.OWLClass;
+import org.semanticweb.owl.model.OWLOntology;
+import org.semanticweb.owl.model.OWLOntologyCreationException;
+import org.semanticweb.owl.model.OWLOntologyManager;
 import org.semanticweb.reasonerfactory.pellet.PelletReasonerFactory;
+
+import Zql.ZConstant;
+import Zql.ZExpression;
 
 // new ends
 /**
- * @author Roshan, Rohit
- * This class has all the methods to call Pellet Reasoner API to
- * find super, sub, equivalent classes.
+ * @author Roshan, Rohit This class has all the methods to call Pellet
+ * Reasoner API to find super, sub, equivalent classes.
  */
 public class PelletReasonerImpl extends DefaultReasonerImpl {
 
    private static final Logger logger = Logger
          .getLogger(DefaultReasonerImpl.class);
-   
-   
- /* OntologyMapStore is not required incase of Pellet Reasoner as the reasoner
-  * can directly raed the ont map files in owl format
-  */
-   
-  /* OntologyMapStore mapStore = Init._this().getViewContext()
-         .getOntologyMapStore();
 
-   OntologyStore ontStore = Init._this().getViewContext().getOntologyStore(); */
+   /*
+    * OntologyMapStore is not required incase of Pellet Reasoner as the
+    * reasoner can directly raed the ont map files in owl format
+    */
 
+   /*
+    * OntologyMapStore mapStore = Init._this().getViewContext()
+    * .getOntologyMapStore();
+    * 
+    * OntologyStore ontStore =
+    * Init._this().getViewContext().getOntologyStore();
+    */
 
    @Override
-   public Set<URI> getEquivalentClass(OWLReasoner reasoner, OWLClass owlClassObject) {
+   public Set<URI> getEquivalentClass(OWLReasoner reasoner,
+         OWLClass owlClassObject) {
       IndusConfiguration indusConfig = Init._this().getIndusConfiguration();
       String equivalentFlag = indusConfig.getEquivalentFlag();
-      System.out.println("equivalentFlag: " + equivalentFlag);
+      // System.out.println("equivalentFlag: " + equivalentFlag);
       Set<URI> result = new HashSet<URI>();
       Set<OWLClass> equiClsSets;
       try {
          equiClsSets = reasoner.getEquivalentClasses(owlClassObject);
-      for(OWLClass ols : equiClsSets)
-      {                 
+         for (OWLClass ols : equiClsSets) {
             //System.out.println(ols.getURI());
             result.add(ols.getURI());
-      }
-      
-      // If equivalent_flag is set to TRUE, do not take the descendant classes 
-      if ((equivalentFlag != null) && (!equivalentFlag.equalsIgnoreCase("TRUE"))) {
-      Set<Set<OWLClass>> subClsSets;
-      subClsSets = reasoner.getDescendantClasses(owlClassObject);
-      for(OWLClass ols : OWLReasonerAdapter.flattenSetOfSets(subClsSets))
-      {                 
+         }
+
+         // If equivalent_flag is set to TRUE, do not take the descendant classes 
+         if ((equivalentFlag != null)
+               && (!equivalentFlag.equalsIgnoreCase("TRUE"))) {
+            Set<Set<OWLClass>> subClsSets;
+            subClsSets = reasoner.getDescendantClasses(owlClassObject);
+            for (OWLClass ols : OWLReasonerAdapter.flattenSetOfSets(subClsSets)) {
                //System.out.println(ols.getURI());
                result.add(ols.getURI());
-      }
-      } 
+            }
+         }
       } catch (OWLReasonerException e) {
          // TODO Auto-generated catch block
          e.printStackTrace();
@@ -76,21 +77,20 @@ public class PelletReasonerImpl extends DefaultReasonerImpl {
    }
 
    @Override
-   public Set<URI> getSubClass(OWLReasoner reasoner, OWLClass owlClassObject){
+   public Set<URI> getSubClass(OWLReasoner reasoner, OWLClass owlClassObject) {
       // TODO Auto-generated method stub
       Set<URI> result = new HashSet<URI>();
       Set<Set<OWLClass>> subClsSets;
       try {
          subClsSets = reasoner.getDescendantClasses(owlClassObject);
-                        
-         System.out.println("\n Now we are printing the URI's");               
-         
-         for(OWLClass ols : OWLReasonerAdapter.flattenSetOfSets(subClsSets))
-      {                 
-               System.out.println(ols.getURI());
-               result.add(ols.getURI());
-      }
-         System.out.println("\n");
+
+         // System.out.println("\n Now we are printing the URI's");               
+
+         for (OWLClass ols : OWLReasonerAdapter.flattenSetOfSets(subClsSets)) {
+            System.out.println(ols.getURI());
+            result.add(ols.getURI());
+         }
+         //     System.out.println("\n");
 
       } catch (OWLReasonerException e) {
          // TODO Auto-generated catch block
@@ -98,8 +98,7 @@ public class PelletReasonerImpl extends DefaultReasonerImpl {
       }
       // In this case, we don't particularly care about the equivalences, so we will flatten this
       // set of sets and print the result
-         
-         
+
       return result;
    }
 
@@ -107,85 +106,84 @@ public class PelletReasonerImpl extends DefaultReasonerImpl {
    public Set<URI> getSuperClass(OWLReasoner reasoner, OWLClass owlClassObject) {
       Set<URI> result = new HashSet<URI>();
       // TODO Auto-generated method stub
-      Set<Set<OWLClass>> superClsSets;      
-      
+      Set<Set<OWLClass>> superClsSets;
+
       try {
          superClsSets = reasoner.getAncestorClasses(owlClassObject);
-         System.out.println("superClsSets size: "+superClsSets.size());
-            
-      for(OWLClass ols : OWLReasonerAdapter.flattenSetOfSets(superClsSets))
-   {                 
-            System.out.println("URIs: "+ols.getURI());
+         //     System.out.println("superClsSets size: "+superClsSets.size());
+
+         for (OWLClass ols : OWLReasonerAdapter.flattenSetOfSets(superClsSets)) {
+            //     System.out.println("URIs: "+ols.getURI());
             result.add(ols.getURI());
-   }
+         }
       } catch (OWLReasonerException e) {
          // TODO Auto-generated catch block
          e.printStackTrace();
       }
-      System.out.println("result size: "+result.size());
+      //    System.out.println("result size: "+result.size());
       return result;
    }
-   
-  
+
    public ZExpression getAVHClass(URI ontMapURI, URI classID, String AVHRole,
 
    ZExpression inp) {
-      
+
       Set<URI> result = null;
-      
+
       try {
-      OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-      OWLOntology ont = manager.loadOntologyFromPhysicalURI(ontMapURI);
-      System.out.println("Loaded " + ont.getURI());
-      OWLReasonerFactory reasonerFactory = new PelletReasonerFactory();
-      OWLReasoner reasoner = reasonerFactory.createReasoner(manager);
-      Set<OWLOntology> importsClosure = manager.getImportsClosure(ont);
-      System.out.println("importsClosure "+ importsClosure.size());
-      reasoner.loadOntologies(importsClosure);
-      reasoner.classify();
-      
-      boolean consistent = reasoner.isConsistent(ont);
-      System.out.println("Consistent: " + consistent);
-      System.out.println("\n");
-      
-       /*We can easily get a list of inconsistent classes.  (A class is inconsistent if it
-       can't possibly have any instances).  Note that the getInconsistentClasses method
-       is really just a convenience method for obtaining the classes that are equivalent
-       to owl:Nothing. */
-      
-      Set<OWLClass> inconsistentClasses = reasoner.getInconsistentClasses();
-      if (!inconsistentClasses.isEmpty()) {
-          System.out.println("The following classes are inconsistent: ");
-          for(OWLClass cls : inconsistentClasses) {
-              System.out.println("    " + cls);
-          }
-      }
-      else {
-          System.out.println("There are no inconsistent classes");
-      }
-      System.out.println("\n");
+         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+         OWLOntology ont = manager.loadOntologyFromPhysicalURI(ontMapURI);
+         //    System.out.println("Loaded " + ont.getURI());
+         OWLReasonerFactory reasonerFactory = new PelletReasonerFactory();
+         OWLReasoner reasoner = reasonerFactory.createReasoner(manager);
+         Set<OWLOntology> importsClosure = manager.getImportsClosure(ont);
+         //     System.out.println("importsClosure "+ importsClosure.size());
+         reasoner.loadOntologies(importsClosure);
+         reasoner.classify();
 
-      OWLClass owlClassObject = manager.getOWLDataFactory().getOWLClass(URI.create(classID.toString()));
+         boolean consistent = reasoner.isConsistent(ont);
+         //    System.out.println("Consistent: " + consistent);
+         //    System.out.println("\n");
 
-      if (AVHRole.equals(">")) {
-         result = getSuperClass(reasoner, owlClassObject);
-      } else if (AVHRole.equals("<")) {
-         result = getSubClass(reasoner, owlClassObject);
-      } else if (AVHRole.equals("=")) {
-         result = getEquivalentClass(reasoner, owlClassObject);
-      } else {
-         // Will not Happen
-         logger.warn("UNKNOWN AVH ROLE=" + AVHRole);
-         return inp;
-      }
-      }catch(UnsupportedOperationException exception) {
+         /*
+          * We can easily get a list of inconsistent classes. (A class is
+          * inconsistent if it can't possibly have any instances). Note
+          * that the getInconsistentClasses method is really just a
+          * convenience method for obtaining the classes that are
+          * equivalent to owl:Nothing.
+          */
+
+         Set<OWLClass> inconsistentClasses = reasoner.getInconsistentClasses();
+         if (!inconsistentClasses.isEmpty()) {
+            System.out.println("The following classes are inconsistent: ");
+            for (OWLClass cls : inconsistentClasses) {
+               System.out.println("    " + cls);
+            }
+         } else {
+//          System.out.println("There are no inconsistent classes");
+         }
+         //     System.out.println("\n");
+
+         OWLClass owlClassObject = manager.getOWLDataFactory().getOWLClass(
+               URI.create(classID.toString()));
+
+         if (AVHRole.equals(">")) {
+            result = getSuperClass(reasoner, owlClassObject);
+         } else if (AVHRole.equals("<")) {
+            result = getSubClass(reasoner, owlClassObject);
+         } else if (AVHRole.equals("=")) {
+            result = getEquivalentClass(reasoner, owlClassObject);
+         } else {
+            // Will not Happen
+            logger.warn("UNKNOWN AVH ROLE=" + AVHRole);
+            return inp;
+         }
+      } catch (UnsupportedOperationException exception) {
          System.out.println("Unsupported reasoner operation.");
-      }
-      catch(OWLReasonerException ex) {
-          System.out.println("Reasoner error: " + ex.getMessage());
-      }
-      catch (OWLOntologyCreationException e) {
-          System.out.println("Could not load the pizza ontology: " + e.getMessage());
+      } catch (OWLReasonerException ex) {
+         System.out.println("Reasoner error: " + ex.getMessage());
+      } catch (OWLOntologyCreationException e) {
+         System.out.println("Could not load the  ontology: " + e.getMessage());
       }
 
       ZConstant value = null;
@@ -200,7 +198,7 @@ public class PelletReasonerImpl extends DefaultReasonerImpl {
       } else {
          //no Sub/Super/EquivalentClass exist (e.g. the case when there are no mappings)
          //temporary solution is fill it with a value which does not exist. In Future you can optimize it
-        // logger.trace("NOTHING FOR: " + classID + " " + AVHRole
+         // logger.trace("NOTHING FOR: " + classID + " " + AVHRole
          //      + "  in data source: " + targetDataSource);
          value = new ZConstant("XX_NOT_FOUND_XX", ZConstant.STRING);
          inp.addOperand(value);
